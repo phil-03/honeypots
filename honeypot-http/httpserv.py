38eba156806c3d77c6dbe65d2148ba9638146f34
phil-03 <phillyphil0314@gmail.com>
2021-05-03 20:31:48 -0400

    multi-thread

--- honeypot-http/httpserv.py
+++ honeypot-http/httpserv.py
@@ -15,6 +15,8 @@ import SocketServer
 import os
 import logging
 
+import time, threading, socket, SocketServer, BaseHTTPServer
+
 BIND_HOST = 'localhost'
 PORT = 8000
 
@@ -51,7 +53,48 @@ class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         logging.error(self.headers)
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_POST(self)
 
+'''class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
+    def do_GET(self):
+        if self.path != '/':
+            self.send_error(404, "Object not found")
+            return
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        # serve up an infinite stream
+        i = 0
+        while True:
+            self.wfile.write("%i " % i)
+            time.sleep(0.1)
+            i += 1
+
+# Create ONE socket.
+addr = ('', 8000)
+sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(addr)
+sock.listen(5)
+
+# Launch 100 listener threads.
+class Thread(threading.Thread):
+    def __init__(self, i):
+        threading.Thread.__init__(self)
+        self.i = i
+        self.daemon = True
+        self.start()
+    def run(self):
+        httpd = BaseHTTPServer.HTTPServer(addr, Handler, False)
+
+        # Prevent the HTTP server from re-binding every handler.
+        # https://stackoverflow.com/questions/46210672/
+        httpd.socket = sock
+        httpd.server_bind = self.server_close = lambda self: None
+
+        httpd.serve_forever()
+[Thread(i) for i in range(100)]
+time.sleep(9e9) '''
 
 
 Handler = GetHandler
