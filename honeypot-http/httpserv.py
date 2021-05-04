5af334acd7de521d361a39f6798f24d60b4f4f8f
Philip <phillyphil0314@gmail.com>
2021-05-03 20:20:51 -0700

    http

--- honeypot-http/httpserv.py
+++ honeypot-http/httpserv.py
@@ -10,60 +10,64 @@
 
 
 #Handle Python2/3 import error
-try:
+'''try:
    
     import http.server as SimpleHTTPServer
     import http.server as BaseHTTPServer
     import http.server as BaseHTTPRequestHandler
     import socketserver as SocketServer
-    import socketserver as ThreadingMixIn
-    import threading
 except ImportError:
     
     import SimpleHTTPServer
     import BaseHTTPServer
     import SocketServer
-
-#from http.server import SimpleHTTPServer, BaseHTTPRequestHandler
+'''
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from sys import argv
-#import SocketServer
+from socketserver import ThreadingMixIn
 import os
+import sys
 import logging
+import threading
 
 
 USE_HTTPS = True
-BIND_HOST = 'localhost'
+BIND_HOST = '0.0.0.0'
 PORT = 8000
 
-web_directory = os.path.join(os.path.dirname(__file__), 'data')#find html page?
-os.chdir(web_directory)
+#web_directory = os.path.join(os.path.dirname(__file__), 'data')#find html page?
+#os.chdir(web_directory)
 
 #Start out with simple http webserver
 
-class GetHandler(http.server.SimpleHTTPRequestHandler):#SimpleHTTPServer
+class GetHandler(BaseHTTPRequestHandler):#SimpleHTTPServer.SimpleHTTPRequestHandler
 
     def do_GET(self):
-        self.write_response(b'')
         #if self.path == '/':
             #self.path = 'mywebpage.html'
         logging.error(self.headers)
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-
+        self.send_response(200)
+        self.end_headers()
+        #self.wfile.write(b'Hello Bro\t' +threading.currentThread().getName().encode() + b'\t')
+        self.wfile.write(bytes("<html><head><p>Parse error: syntax error, unexpected end of file in /home/pbutts/public_html/wp-content/themes/bettycommerce/includes/pro_framework/local.php on line 812</p></head>/html>","utf-8")) 
+       
+    '''
     def do_POST(self): #Listen for POST req
         content_length = int(self.header.get('content-length', 0)) #echo out header
         body = self.rfile.read(content_length) #echo body
-        self.write_response(body)
+        #self.write_response(body)
         logging.error(self.headers)
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_POST(self)
+    '''
 
-    def write_response(self, content):
+    '''def write_response(self, content):
         self.send_response(200)
-        self.end_headers()
+        self.end_hees/pro_framework/local.php on line 812</p></head>aders()
         self.wfile.write(content)
 
-        print(self.headers)
+        print(self.headeres/pro_framework/local.php on line 812</p></head>s)es/pro_framework/local.php on line 812</p></head>
         print(content.decode('utf-8')) #Print info back to console
-
+    '''
 
 
 #For continuous streaming it is better to use sockets via BaseHTTPServer
@@ -71,33 +75,24 @@ class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
     pass
 
 
-#Handler = GetHandler
-#httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-
-
+'''
 #Parse cmdline args
 if len(argv) > 1:
     arg = argv[1].split(':')
     BIND_HOST = arg[0]
     PORT = int(arg[1])
+'''
 
-
-#Start Server
-#printf(f'Listening on http://{BIND_HOST}:{PORT}\n')
-#buffer=1
-#sys.stderr = open('http.log', 'w', buffer)
-#httpd = HTTPServer((BIND_HOST, PORT), SimpleHTTPRequestHandler)
-#httpd.serve_forever()
 
 def runServer():
-    #printf(f'Listening on http://{BIND_HOST}:{PORT}\n')
+    printf(f'Listening on http://{BIND_HOST}:{PORT}\n')
     buffer=1
     sys.stderr = open('http.log', 'w', buffer)
-    server = ThreadingSimpleServer(('0.0.0.0', 4444), GetHandler)
-    if USE_HTTPS:
-        import ssl
-        server.socket = ssl.wrap_socket(server.socket, keyfile='./key.pem', certfile='./cert.pem', server_side=True)
+    server = ThreadingSimpleServer((BIND_HOST, PORT), GetHandler)
+    #if USE_HTTPS:
+     #   import ssl
+      #  server.socket = ssl.wrap_socket(server.socket, keyfile='./key.pem', certfile='./cert.pem', server_side=True)
     server.serve_forever()
 
 #Establish self-signed cert: openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365 
